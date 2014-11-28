2368f6d57a20e80955ac530ac54fff462fd2c359
gcmalloc <gcmalloc@gmail.com>
2014-11-28 14:19:31 +0100

    set the default version and provide a sane default

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,15 +25,28 @@ class minecraft (
   $user          = 'minecraft',
   $group         = 'minecraft',
   $homedir       = '/opt/minecraft',
-  $version       = '1.8.1',
+  $version       = undef,
   $manage_java   = true,
   $heap_size     = 2048,
   $heap_start    = 512,
   $eula_accepted = true,
+  $use_bukkit    = false,
 )
 {
-  $url = "https://s3.amazonaws.com/Minecraft.Download/versions/${version}/minecraft_server.${version}.jar"
-  $jar_file = "${homedir}/minecraft${version}.jar"
+  if ($version) {
+    $real_version = $version
+  } else {
+    $real_version = $use_bukkit ? {
+      true => '02388_1.6.4-R2.0',
+      false => '1.8.1',
+    }
+  }
+
+  $url = $use_bukkit ? {
+    true  => "https://s3.amazonaws.com/Minecraft.Download/versions/${real_version}/minecraft_server.${real_version}.jar",
+    false => "https://dl.bukkit.org/downloads/bukkit/get/${real_version}/bukkit.jar"
+  }
+  $jar_file = "${homedir}/minecraft${real_version}.jar"
   $exec = "java -Xmx${heap_size}M -Xms${heap_start}M -jar ${jar_file} nogui"
   ensure_packages(['wget'])
 
@@ -51,7 +64,7 @@ class minecraft (
     managehome => true,
   }
 
-  exec {"download minecraft jar version ${version}":
+  exec {"download minecraft jar version ${real_version}":
     command => "/usr/bin/wget ${url} -O ${jar_file}",
     user    => $user,
     creates => $jar_file,
